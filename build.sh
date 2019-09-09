89d93f9f03f1544edf033e838e126ba701285271
Richard Marques <richardmarques@MacBook-Pro-de-Richard.local>
2019-09-09 18:16:46 -0300

    ajustes

--- build.sh
+++ build.sh
@@ -2,43 +2,68 @@
 cd eclipse-workspace/SpringBootAdmin
 ./mvnw package -Dmaven.test.skip=true
 cd target
-cp *.jar ../springAdmin/app.jar
-cd ..
+cp *.jar ../../../springAdmin/app.jar
+cd ../../..
 
 # build de conta corrente
 cd eclipse-workspace/conta-corrente
 ./mvnw package -Dmaven.test.skip=true
 cd target
-cp *.jar ../conta-corrente/app.jar
-cd ..
+cp *.jar ../../../conta-corrente/app.jar
+cd ../../..
 
 # build de saldoKafka
 cd eclipse-workspace/saldoKafka
 ./mvnw package -Dmaven.test.skip=true
 cd target
-cp *.jar ../saldokafka/app.jar
-cd ..
+cp *.jar ../../../saldokafka/app.jar
+cd ../../..
 
 # build de senha
 cd eclipse-workspace/senha
 ./mvnw package -Dmaven.test.skip=true
 cd target
-cp *.jar ../senha/app.jar
-cd ..
+cp *.jar ../../../senha/app.jar
+cd ../../..
 
 # build de senhaKafka
 cd eclipse-workspace/senhaKafka
 ./mvnw package -Dmaven.test.skip=true
 cd target
-scp *.jar ../senhakafka/app.jar
-cd ..
+cp *.jar ../../../senhakafka/app.jar
+cd ../../..
 
 # build de transferencia
-cd /Users/richardmarques/eclipse-workspace/transferencia
+cd eclipse-workspace/transferencia
 ./mvnw package -Dmaven.test.skip=true
 cd target
-cp *.jar ../transferencia/app.jar
-cd ..
+cp *.jar ../../../transferencia/app.jar
+cd ../../..
+
+cd conta-corrente
+./docker.sh
+cd ../
+
+cd saldokafka
+./docker.sh
+cd ../
+
+cd senha
+./docker.sh
+cd ../
+
+cd senhakafka
+./docker.sh
+cd ../
+
+cd springAdmin
+./docker.sh
+cd ../
+
+cd transferencia
+./docker.sh
+cd ../
+
 
 kubectl apply -f kafka.yaml
 kubectl apply -f mysql.yaml
